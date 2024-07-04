0ff28335dea99e1fed6f79f84720144df546ebe3
Paulo Henrique <ph.1928@hotmail.com>
2024-07-04 11:30:55 -0300

    Ajusta output do pagamento com paypal

--- src/Ex5/PagamentoPayPal.java
+++ src/Ex5/PagamentoPayPal.java
@@ -3,6 +3,6 @@ package Ex5;
 public class PagamentoPayPal implements Pagamento{
     @Override
     public void pagar(double valor) {
-        System.out.println("Pagamento efetuado com PayPal");
+        System.out.println("Pagamento de R$ " + valor + " efetuado com PayPal");
     }
 }
