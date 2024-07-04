06a9ad8cce505a5aee371e14325dab5cc0643520
Paulo Henrique <ph.1928@hotmail.com>
2024-07-04 09:25:38 -0300

    WIP: Exercício 4

---
+++ src/Ex4/Impressora.java
@@ -0,0 +1,9 @@
+package Ex4;
+
+public interface Impressora {
+    void imprimirDocumento();
+
+    void escanearDocumento();
+
+    void faxearDocumento();
+}
---
+++ src/Ex4/Main.java
@@ -0,0 +1,9 @@
+package Ex4;
+
+public class Main {
+    public static void main(String[] args) {
+        // Interface Segregation Principle
+
+
+    }
+}
